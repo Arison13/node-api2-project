2b8d04f8586a5ee8d324be740125382f22841885
Arison13 <arisonarias@gmail.com>
2021-11-30 15:15:46 -0500

    can get all users from the first endpoint

--- api/server.js
+++ api/server.js
@@ -1,2 +1,22 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const server = express();
+const Post = require('./posts/posts-model')
+
+server.get('/api/posts', (req, res)=> {
+    Post.find()
+    .then(post => {
+        res.status(200).json(post);
+    })
+    .catch(error => {
+        res.status(500).json({
+          message: 'Error retrieving the dogs',
+          error
+        });
+      });
+})
+
+server.use(express.json())
+
+module.exports = server;
