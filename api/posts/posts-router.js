b55e751e91946f62c9df0c257418f2eaf4027a8e
Arison13 <arisonarias@gmail.com>
2021-11-30 15:32:50 -0500

    transfer code to the right place

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,19 @@
 // implement your posts router here
+const express = require('express')
+const Post = require("./posts-model")
+const router = express.Router()
+
+router.get('/', (req, res)=> {
+    Post.find()
+    .then(post => {
+        res.status(200).json(post);
+    })
+    .catch(error => {
+        res.status(500).json({
+          message: 'Error retrieving the dogs',
+          error
+        });
+      });
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -2,21 +2,36 @@
 // require your posts router and connect it here
 const express = require('express');
 const server = express();
-const Post = require('./posts/posts-model')
-
-server.get('/api/posts', (req, res)=> {
-    Post.find()
-    .then(post => {
-        res.status(200).json(post);
-    })
-    .catch(error => {
-        res.status(500).json({
-          message: 'Error retrieving the dogs',
-          error
-        });
-      });
-})
-
+const postsRouter = require('./posts/posts-router')
+//middleware to parse through JSON //####NEVER FORGETTT!!!!!
 server.use(express.json())
 
+server.use('/api/posts', postsRouter);
+
+// server.get('/api/posts', (req, res)=> {
+//     Post.find()
+//     .then(post => {
+//         res.status(200).json(post);
+//     })
+//     .catch(error => {
+//         res.status(500).json({
+//           message: 'Error retrieving the dogs',
+//           error
+//         });
+//       });
+// })
+
+
+// server.get('/api/posts/:id', (req, res)=> {
+//     Post.findById(req.params.id)
+//     .then(post => {
+//         res.status(200).json(post);
+//     })
+//     .catch(error => {
+//         res.status(500).json({
+//           message: 'Error retrieving the dogs',
+//           error
+//         });
+//       });
+// })
 module.exports = server;
